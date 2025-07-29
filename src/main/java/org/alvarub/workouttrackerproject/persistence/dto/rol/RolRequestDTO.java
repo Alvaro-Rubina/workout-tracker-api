cb3be3b4ef96e6d8180f06753ad78c9f000cae9b
Alvaro Rubina <alvarorubina132@gmail.com>
2025-07-28 21:55:55 -0300

    Creado request DTO para Rol

---
+++ src/main/java/org/alvarub/workouttrackerproject/persistence/dto/rol/RolRequestDTO.java
@@ -0,0 +1,20 @@
+package org.alvarub.workouttrackerproject.persistence.dto.rol;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RolRequestDTO {
+
+    @NotBlank(message = "El campo name es obligatorio")
+    private String name;
+
+    @NotBlank(message = "El campo description es obligatorio")
+    private String description;
+}
