d5a098db507bd7a9cb9dc8f80d81385316beb423
Alvaro Rubina <alvarorubina132@gmail.com>
2025-08-02 21:42:06 -0300

    Creado GlobalExceptionHandler y NotFoundException

---
+++ src/main/java/org/alvarub/workouttrackerproject/exception/GlobalExceptionHandler.java
@@ -0,0 +1,36 @@
+package org.alvarub.workouttrackerproject.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception ex) {
+        ErrorResponse error = ErrorResponse.builder()
+                .timestamp(LocalDateTime.now())
+                .message("Error interno del servidor")
+                .details(List.of(ex.getMessage()))
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleResourceNotFoundException(NotFoundException ex) {
+        ErrorResponse error = ErrorResponse.builder()
+                .timestamp(LocalDateTime.now())
+                .status(HttpStatus.NOT_FOUND.value())
+                .message(ex.getMessage())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+}
---
+++ src/main/java/org/alvarub/workouttrackerproject/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package org.alvarub.workouttrackerproject.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
