3918a4ba41358a400e57bfbd8ff33a8769118a51
Alvaro Rubina <alvarorubina132@gmail.com>
2025-08-08 15:14:31 -0300

    Añadidos metodos personalizados a UsuarioRepository y AgendaRepository

--- src/main/java/org/alvarub/workouttrackerproject/persistence/repository/AgendaRepository.java
+++ src/main/java/org/alvarub/workouttrackerproject/persistence/repository/AgendaRepository.java
@@ -1,7 +1,11 @@
 package org.alvarub.workouttrackerproject.persistence.repository;
 
 import org.alvarub.workouttrackerproject.persistence.entity.Agenda;
+import org.alvarub.workouttrackerproject.persistence.entity.Rutina;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AgendaRepository extends JpaRepository<Agenda, Long> {
+    List<Agenda> findAllByRoutine(Rutina rutina);
 }
--- src/main/java/org/alvarub/workouttrackerproject/persistence/repository/UsuarioRepository.java
+++ src/main/java/org/alvarub/workouttrackerproject/persistence/repository/UsuarioRepository.java
@@ -1,7 +1,17 @@
 package org.alvarub.workouttrackerproject.persistence.repository;
 
+import org.alvarub.workouttrackerproject.persistence.entity.Rutina;
 import org.alvarub.workouttrackerproject.persistence.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-  }
+
+  Optional<Usuario> findByCreatedRoutinesContains(Rutina rutina);
+
+  List<Usuario> findAllByLikedRoutinesContains(Rutina rutina);
+
+  List<Usuario> findAllBySavedRoutinesContains(Rutina rutina);
+}
