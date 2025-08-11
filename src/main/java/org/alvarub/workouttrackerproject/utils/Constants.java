96b9dc388309c3210dc29e2d93c3fa3761a09e90
Alvaro Rubina <alvarorubina132@gmail.com>
2025-08-11 00:42:38 -0300

    Añadidas contantes a la clase Constants

--- src/main/java/org/alvarub/workouttrackerproject/utils/Constants.java
+++ src/main/java/org/alvarub/workouttrackerproject/utils/Constants.java
@@ -3,5 +3,6 @@ package org.alvarub.workouttrackerproject.utils;
 public class Constants {
 
     public static final String DEFAULT_CATEGORY_NAME = "General";
-
+    public static final String ADMIN_ROL_NAME = "Administrador";
+    public static final String USER_ROL_NAME = "Usuario";
 }
