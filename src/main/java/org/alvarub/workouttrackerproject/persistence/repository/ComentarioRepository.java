a7946b3f45abb3f5639cfca334aad5cce4bac07f
Alvaro Rubina <alvarorubina132@gmail.com>
2025-08-13 00:11:27 -0300

    Añadidos metodos personalizados al ComentarioRepository

--- src/main/java/org/alvarub/workouttrackerproject/persistence/repository/ComentarioRepository.java
+++ src/main/java/org/alvarub/workouttrackerproject/persistence/repository/ComentarioRepository.java
@@ -3,5 +3,9 @@ package org.alvarub.workouttrackerproject.persistence.repository;
 import org.alvarub.workouttrackerproject.persistence.entity.Comentario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ComentarioRepository extends JpaRepository<Comentario, Long> {
+    List<Comentario> findAllByRoutineId(Long routineId);
+    List<Comentario> findAllByUserId(Long userId);
 }
