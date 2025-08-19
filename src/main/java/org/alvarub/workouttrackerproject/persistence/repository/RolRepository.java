490a25cbb83c682f702758f4334c28a7268c15b4
Alvaro Rubina <alvarorubina132@gmail.com>
2025-08-19 08:45:23 -0300

    Agregados metodos en los repositorios para Usuario y Rol

--- src/main/java/org/alvarub/workouttrackerproject/persistence/repository/RolRepository.java
+++ src/main/java/org/alvarub/workouttrackerproject/persistence/repository/RolRepository.java
@@ -3,6 +3,10 @@ package org.alvarub.workouttrackerproject.persistence.repository;
 import org.alvarub.workouttrackerproject.persistence.entity.Rol;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RolRepository extends JpaRepository<Rol, Long> {
-    public boolean existsByNameIgnoreCase(String name);
+    boolean existsByNameIgnoreCase(String name);
+
+    Optional<Rol> findByName(String name);
 }
