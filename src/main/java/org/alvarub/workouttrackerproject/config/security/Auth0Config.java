5f939eaff5728951c544b266365bb2a17177f391
Alvaro Rubina <alvarorubina132@gmail.com>
2025-08-19 08:14:10 -0300

    Creadas clases de configuracion de seguridad

---
+++ src/main/java/org/alvarub/workouttrackerproject/config/security/Auth0Config.java
@@ -0,0 +1,31 @@
+package org.alvarub.workouttrackerproject.config.security;
+
+import com.auth0.client.auth.AuthAPI;
+import com.auth0.client.mgmt.ManagementAPI;
+import com.auth0.exception.Auth0Exception;
+import com.auth0.json.auth.TokenHolder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.auth0.net.AuthRequest;
+
+@Configuration
+public class Auth0Config {
+
+    @Value("${auth0.domain}")
+    private  String domain;
+
+    @Value("${auth0.clientId}")
+    private  String clientId;
+
+    @Value("${auth0.clientSecret}")
+    private  String clientSecret;
+
+    @Bean
+    public ManagementAPI managementAPI() throws Auth0Exception {
+        AuthAPI authAPI = new AuthAPI(domain, clientId, clientSecret);
+        AuthRequest request = authAPI.requestToken("https://" + domain + "/api/v2/");
+        TokenHolder holder = request.execute();
+        return new ManagementAPI(domain, holder.getAccessToken());
+    }
+}
