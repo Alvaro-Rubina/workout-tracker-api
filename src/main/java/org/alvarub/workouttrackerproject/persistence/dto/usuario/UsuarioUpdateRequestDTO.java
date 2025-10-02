2050ebf012be46fcf0b9153ae04480c1d0d48fe4
Alvaro Rubina <145699883+Alvaro-Rubina@users.noreply.github.com>
2025-10-02 16:24:49 -0300

    Creado DTO para actualizar campos basicos de un usuario

---
+++ src/main/java/org/alvarub/workouttrackerproject/persistence/dto/usuario/UsuarioUpdateRequestDTO.java
@@ -0,0 +1,19 @@
+package org.alvarub.workouttrackerproject.persistence.dto.usuario;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UsuarioUpdateRequestDTO {
+
+    private String name;
+
+    private String password;
+
+    private String picture;
+}
